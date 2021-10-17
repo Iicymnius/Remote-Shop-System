b3ab1ee8b381fc14fdddc8a910367a5d5c2c086d
Licymnius <licymnius@outlook.com>
2021-10-17 09:05:19 +0300

    Update cmd.cpp

--- SVN/Server/game/src/cmd.cpp
+++ SVN/Server/game/src/cmd.cpp
@@ -10,10 +10,10 @@ ACMD(do_open_range_npc);
 
 //find again;
 
-	{ "do_clear_affect",		do_clear_affect,		0,	POS_DEAD,		GM_LOW_WIZARD	},
+			{ "do_clear_affect",		do_clear_affect,		0,	POS_DEAD,		GM_LOW_WIZARD	},
 
 //add below;
 
 #ifdef ENABLE_REMOTE_SHOP_SYSTEM
-	{"open_range_npc",		do_open_range_npc,		0,	POS_DEAD,		GM_PLAYER	},
+			{"open_range_npc",		do_open_range_npc,		0,	POS_DEAD,		GM_PLAYER	},
 #endif
