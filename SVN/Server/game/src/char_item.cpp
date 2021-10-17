e970a5fbc00b73bc6a0a68b43288dd0f33c690d4
Licymnius <licymnius@outlook.com>
2021-10-17 05:10:30 +0300

    Update char_item.cpp

--- SVN/Server/game/src/char_item.cpp
+++ SVN/Server/game/src/char_item.cpp
@@ -5,7 +5,7 @@
 //add above;
 
 #ifdef ENABLE_REMOTE_SHOP_SYSTEM
-			case 84001:
+			case 84001://Change. Type -> ITEM_USE - SubType -> USE_SPECIAL
 				ChatPacket(CHAT_TYPE_COMMAND, "openremoteshop");
 				break;
 #endif
